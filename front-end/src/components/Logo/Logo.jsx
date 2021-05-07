8ee4fbbfcae7af55e9d5259bf1c8ebb6e104cf21
Charles Lu <kuanminglu0529@gmail.com>
2021-05-06 20:07:39 -0700

    fixed icon relative path pointing to wrong place

--- front-end/src/components/Logo/Logo.jsx
+++ front-end/src/components/Logo/Logo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../assets/images/logo/logo.svg";
+import logo from "../../assets/logo/logo.svg";
 import classes from "./logo.module.scss";
 
 function Logo() {
